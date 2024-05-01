1eca668ee74614fe4ef3e0bd7a3971c741d105c5
maxi <maximo.colombatto@gmail.com>
2024-05-01 18:30:53 -0300

    deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
-
 import { SistemaProvider } from "./context/SistemaContext";
 
 import "./App.css";
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App.jsx'
 import './index.css'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
